c96a77a5d02725cff90e58a615e20e84e110b0be
Alex Walker <alex.walker@thedatalab.org>
2020-03-29 16:45:32 +0100

    Add in variable for CVD meds

--- notebooks/diffable_python/cvd_covariate.py
+++ notebooks/diffable_python/cvd_covariate.py
@@ -14,9 +14,13 @@
 #     name: python3
 # ---
 
+import os
 import pyodbc
 import pandas as pd
 from IPython.display import display, Markdown
+from ebmdatalab import bq
+
+# ### Server connection
 
 server = 'covid.ebmdatalab.net,1433'
 database = 'OPENCoronaExport' 
@@ -25,10 +29,42 @@ password = 'ahsjdkaJAMSHDA123['
 cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor = cnxn.cursor()
 
+# ### CHD codes
+# - just those in the CHD cluster, to identify cardiac events
+# - in order to differentiate between patients on primary and secondary prevention
+
 qof_clusters = pd.read_csv('../data/QoFClusteres_CTV3Codes - Sheet1.csv')
 chd_codes = qof_clusters.loc[qof_clusters['ClusterId']=='CHD','CTV3Code']
 chd_codes.head()
 
+# ### CVD medicine codes
+# - taken from https://github.com/ebmdatalab/cvd-covid-codelist-notebook/blob/master/notebooks/cvd.codelist.ipynb
+
+# +
+sql = '''
+WITH bnf_codes AS (
+  SELECT bnf_code FROM hscic.presentation WHERE 
+    bnf_code LIKE '02%' #BNF cvd chapter 
+)
+
+SELECT "vmp" AS type, id, bnf_code, nm
+FROM dmd.vmp
+WHERE bnf_code IN (SELECT * FROM bnf_codes)
+
+UNION ALL
+
+SELECT "amp" AS type, id, bnf_code, descr
+FROM dmd.amp
+WHERE bnf_code IN (SELECT * FROM bnf_codes)
+
+ORDER BY type, bnf_code, id
+'''
+
+cvd_medcodes = bq.cached_read(sql, csv_path=os.path.join('..','data','cvd_medcodes.csv'))
+cvd_medcodes.head()
+
+
+# -
 
 def codes_to_sql_where(col_name, code_list):
     where = ""
@@ -46,10 +82,36 @@ codes_to_sql_where("CTV3Code", chd_codes.head())
 
 codes_where = codes_to_sql_where("CTV3Code", chd_codes)
 query = f'''
-SELECT Patient_ID
+SELECT DISTINCT Patient_ID, 1 AS chd_code
 FROM CodedEvent
 WHERE {codes_where}
+ORDER BY Patient_ID
 '''
-df = pd.read_sql(query, cnxn, index_col='Patient_ID')
-df['chd'] = True
-df
+clin_df = pd.read_sql(query, cnxn, index_col='Patient_ID')
+clin_df
+
+codes_where = codes_to_sql_where("DMD_ID", cvd_medcodes['id'])
+query = f'''
+SELECT 
+  med.Patient_ID,
+  COUNT(med.Patient_ID) AS cvd_meds
+
+FROM MedicationDictionary AS dict
+
+INNER JOIN MedicationIssue AS med
+ON dict.MultilexDrug_ID = med.MultilexDrug_ID
+
+WHERE ({codes_where})
+
+GROUP BY med.Patient_ID
+
+ORDER BY med.Patient_ID
+'''
+med_df = pd.read_sql(query, cnxn, index_col='Patient_ID')
+med_df.head()
+
+clin_df.join(med_df, how='outer').fillna(0).head()
+
+# ### Connection should be closed before restarting the kernal or closing the notebook
+
+cnxn.close()
