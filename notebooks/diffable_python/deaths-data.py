a012fdfa81e59069cfdd3ddc68d03d8aea123660
Seb Bacon <seb.bacon@gmail.com>
2020-04-20 12:13:46 +0100

    Dedicated outcomes notebook

---
+++ notebooks/diffable_python/deaths-data.py
@@ -0,0 +1,35 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: all
+#     notebook_metadata_filter: all,-language_info
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.3.3
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+import pyodbc
+import pandas as pd
+from IPython.display import display, Markdown
+
+server = 'covid.ebmdatalab.net,1433'
+database = 'OPENCoronaExport' 
+username = 'SA'
+password = 'ahsjdkaJAMSHDA123[' 
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+cursor = cnxn.cursor()
+
+pd.read_sql("select top 10 * from CPNS", cnxn).iloc[0]
+
+pd.read_sql("select top 10 * from SGSS_Negative", cnxn).iloc[0]
+
+pd.read_sql("select top 10 * from SGSS_Positive", cnxn).iloc[0]
+
+pd.read_sql("select top 10 * from ONS_Deaths", cnxn).iloc[0]
+
