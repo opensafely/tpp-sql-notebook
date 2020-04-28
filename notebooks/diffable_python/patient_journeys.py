0a2bf188e60464c9b958a12fa8c311f43e63533c
HelenCEBM <helencebm@gmail.com>
2020-04-28 18:39:58 +0100

    Created patient_journeys notebook (WIP)
    Also small tweaks in ECDS script

--- notebooks/diffable_python/patient_journeys.py
+++ notebooks/diffable_python/patient_journeys.py
@@ -28,22 +28,57 @@ password = 'ahsjdkaJAMSHDA123['
 cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor = cnxn.cursor()
 
-# ### Inspect columns
+# ## Total patients included 
 
 # +
-# increase column display limit so that we can see all rows in summary data
-sql = '''-- main ecds table
-select TOP 1 *
-from ECDS
+sql = '''-- all patients in primary care:
+select DISTINCT Patient_ID
+from Patient
 '''
+tpp = pd.read_sql(sql, cnxn)
 
-ecds = pd.read_sql(sql, cnxn)
-pd.set_option('display.max_rows', 200)
+display(Markdown(f"**TPP Summary**"))
+print("No of registered patients: ", tpp["Patient_ID"].nunique())
 
-pd.Series(ecds.columns)
 # -
 
-# ### Main ECDS table
+# ## Positive & negative tests (SGSS)
+
+# +
+sql = '''-- SGSS positives:
+select Patient_ID, max(Lab_Report_Date) as pos_Lab_Report_Date_latest, count(*) AS positives
+from SGSS_Positive
+group by Patient_ID
+'''
+p = pd.read_sql(sql, cnxn)
+p["positive_flag"] = 1
+p["pos_Lab_Report_Date_latest"] = pd.to_datetime(p["pos_Lab_Report_Date_latest"])
+
+sql = '''
+-- SGSS negatives:
+select Patient_ID, max(Lab_Report_Date) as neg_Lab_Report_Date_latest, count(*) AS negatives
+from SGSS_Negative
+group by Patient_ID
+'''
+
+n = pd.read_sql(sql, cnxn)
+n["negative_flag"] = 1
+n["neg_Lab_Report_Date_latest"] = pd.to_datetime(n["neg_Lab_Report_Date_latest"])
+
+sgss = p.merge(n, on="Patient_ID", how="outer")
+
+
+display(Markdown(f"**SGSS Summary**"))
+print("Latest lab result date: ", sgss["pos_Lab_Report_Date_latest"].max())
+
+print("Total tests: ", int(sgss["positives"].sum()+sgss["negatives"].sum()))
+print("No of patients tested: ", sgss["Patient_ID"].nunique())
+
+print("No of patients with positives: ", int(sgss["positive_flag"].sum()))
+print("No of patients with negatives: ", int(sgss["negative_flag"].sum()))
+# -
+
+# ## ECDS (A&E)
 
 # +
 sql = '''-- main ecds table
@@ -59,15 +94,7 @@ ecds["ecds_flag"] = 1
 ecds["admit_flag"] = np.where(pd.notnull(ecds["EC_admit_date_latest"]), 1, 0)
 ecds["EC_Conclusion_Date_latest"] = pd.to_datetime(ecds["EC_Conclusion_Date_latest"])
 
-display(Markdown(f"**ECDS Summary**"))
-print("No of patients: ", ecds["Patient_ID"].nunique())
-print("No of patients admitted: ", ecds["admit_flag"].sum())
-print("Latest date: ", ecds["EC_Conclusion_Date_latest"].max())
-# -
-
-# ### ECDS diagnosis
-
-# +
+# diagnoses
 sql = '''-- ecds diagnoses:
 select distinct Patient_ID
 from ECDS_EC_Diagnoses
@@ -77,53 +104,27 @@ where LEFT(DiagnosisCode,15) = '124075100000010'
 d = pd.read_sql(sql, cnxn)
 d["diagnosis_flag"] = 1
 
-display(Markdown(f"**ECDS Diagnosis Summary**"))
-print("No of patients: ", d["Patient_ID"].nunique())
 
-## empty table
-# -
+ecds = ecds.merge(d, on="Patient_ID", how="outer")
 
-# ### Positive in SGSS
+ecds["patients_with_diagnosis_no_ae"] = np.where((ecds["diagnosis_flag"] == 1)&(ecds["ecds_flag"] == 0), 1, 0)
+ecds["patients_with_covid_diagnosis"] = np.where((ecds["diagnosis_flag"] == 1)&(ecds["ecds_flag"] == 1), 1, 0)
 
-# +
-sql = '''-- SGSS positives:
-select Patient_ID, max(Lab_Report_Date) as pos_Lab_Report_Date_latest, count(*) AS positives
-from SGSS_Positive
-group by Patient_ID
-'''
-p = pd.read_sql(sql, cnxn)
-p["positive_flag"] = 1
-p["pos_Lab_Report_Date_latest"] = pd.to_datetime(p["pos_Lab_Report_Date_latest"])
-
-display(Markdown(f"**SGSS Positive Summary**"))
-print("No of patients: ", p["Patient_ID"].nunique())
-print("Latest lab result date: ", p["pos_Lab_Report_Date_latest"].max())
-# -
-
-# ### Negative in SGSS
-
-# +
-sql = '''
--- SGSS negatives:
-select Patient_ID, max(Lab_Report_Date) as neg_Lab_Report_Date_latest, count(*) AS negatives
-from SGSS_Negative
-group by Patient_ID
-'''
-
-n = pd.read_sql(sql, cnxn)
-n["negative_flag"] = 1
-n["neg_Lab_Report_Date_latest"] = pd.to_datetime(n["neg_Lab_Report_Date_latest"])
-
-display(Markdown(f"**SGSS Negative Summary**"))
-print("No of patients: ", n["Patient_ID"].nunique())
-print("Latest lab result date: ", n["neg_Lab_Report_Date_latest"].max())
+display(Markdown(f"**ECDS (A&E) Summary**"))
+print("No of patients: ", ecds["Patient_ID"].nunique())
+print("Of which have covid diagnosis: ", ecds["patients_with_covid_diagnosis"].sum())
+print("No of patients with covid diagnosis but no attendance record: ",ecds["patients_with_diagnosis_no_ae"].sum())
+print("No of patients admitted: ", ecds["admit_flag"].sum())
+print("Latest date: ", ecds["EC_Conclusion_Date_latest"].max())
 # -
 
 # ## ICU
+# Need to add ICU deaths information when available
 
 # +
 sql = '''-- icnarc ICU admissions
 select Patient_ID, 
+max(Ventilator) as ventilator,
 max(IcuAdmissionDateTime) as IcuAdmissionDateTime_latest
 from ICNARC
 group by Patient_ID
@@ -135,34 +136,80 @@ icu["IcuAdmissionDateTime_latest"] = pd.to_datetime(icu["IcuAdmissionDateTime_la
 
 display(Markdown(f"**ICNARC (ICU) Summary**"))
 print("No of patients: ", icu["Patient_ID"].nunique())
+print("Number ventilated: ", int(icu["ventilator"].sum()))
 print("Latest date: ", icu["IcuAdmissionDateTime_latest"].max())
+# -
+
+# ## CPNS (deaths in hospital)
+
+# +
+sql = '''-- CPNS deaths
+select Patient_ID, 
+LocationOfDeath, -- e.g. ICU
+DateOfDeath
+from CPNS
+'''
+
+cpns = pd.read_sql(sql, cnxn)
+cpns["cpns_flag"] = 1
+cpns["DateOfDeath"] = pd.to_datetime(cpns["DateOfDeath"])
+
+display(Markdown(f"**CPNS (deaths) Summary**"))
+print("No of patients: ", cpns["Patient_ID"].count(), "(", cpns["Patient_ID"].nunique(), "unique)")
+print("Latest date: ", cpns["DateOfDeath"].max())
+# -
+
+# ### Summarise deaths by location
+
+cpns.groupby(["LocationOfDeath"])[["Patient_ID"]].count()
+
+# ## ONS Deaths
+
+# +
+sql = '''-- ONS deaths
+select Patient_ID, 
+dod
+from ONS_Deaths
+'''
+
+ons = pd.read_sql(sql, cnxn)
+ons["ons_flag"] = 1
+ons["dod"] = pd.to_datetime(ons["dod"])
+
+display(Markdown(f"**ONS (deaths) Summary**"))
+print("No of patients: ", ons["Patient_ID"].count(), "(", ons["Patient_ID"].nunique(), "unique)")
+print("Latest date: ", ons["dod"].max())
+# -
+
+# ### Weekly date cut-offs
+
+# +
+# create list of weekly dates to assess
+
+latest_date = max(ecds["EC_Conclusion_Date_latest"].max(), p["pos_Lab_Report_Date_latest"].max(), icu["IcuAdmissionDateTime_latest"].max())
+
+l = []
+for i in range(7):
+     l.append( [-i-1, latest_date + pd.Timedelta(days=-7*i-7)] )
+
+l = pd.DataFrame(l, columns=["week_no", "date"])
+l
 
 
 # -
 
-# ## How many patients in ECDS have a positive or negative result in SGSS
-
-# **first check how well periods overlap:**
+# # Summary
 
 # +
-def days_ago(DT=datetime(2020,3,3)):
-    return (date.today()-datetime.date(DT)).days
-
-print ("ECDS max date: ", ecds["EC_Conclusion_Date_latest"].max(), ",  ", days_ago(ecds["EC_Conclusion_Date_latest"].max()), " days ago")
-print ("SGSS max date: ", p["pos_Lab_Report_Date_latest"].max(), ",  ", days_ago(p["pos_Lab_Report_Date_latest"].max()), " days ago")
-print ("ICU max admit date: ", icu["IcuAdmissionDateTime_latest"].max(), ",  ", days_ago(icu["IcuAdmissionDateTime_latest"].max()), " days ago")
-
-
-# +
-def merge_data(ecds, p, n, d, icu):
-    e2 = ecds.merge(p, on="Patient_ID", how="outer")
-    e2 = e2.merge(n, on="Patient_ID", how="outer")
-    e2 = e2.merge(d, on="Patient_ID", how="left")
+def merge_data(sgss, ecds, icu, cpns, ons):
+    e2 = sgss.merge(ecds, on="Patient_ID", how="outer")
     e2 = e2.merge(icu, on="Patient_ID", how="outer")
-
+    e2 = e2.merge(cpns, on="Patient_ID", how="outer")
+    e2 = e2.merge(ons, on="Patient_ID", how="outer")
+    
     # replace nulls:
     # first for date columns
-    cols = ["EC_Conclusion_Date_latest","pos_Lab_Report_Date_latest","neg_Lab_Report_Date_latest", "IcuAdmissionDateTime_latest", "EC_admit_date_latest"]
+    cols = ["EC_Conclusion_Date_latest","pos_Lab_Report_Date_latest","neg_Lab_Report_Date_latest", "IcuAdmissionDateTime_latest", "EC_admit_date_latest", "DateOfDeath", "dod"]
     for c in cols:
         e2[c].fillna(datetime(1900,1,1), inplace=True)
     # other columns
@@ -180,13 +227,13 @@ def merge_data(ecds, p, n, d, icu):
     e2["diagnosis_and_positive_flag"] = np.where(e2["positive_flag"]+e2["diagnosis_flag"]==2, 1, 0)
 
     # patient counts and percentage calculations
-    ecds_patient_count = e2["ecds_flag"].sum()
     sgss_patient_count = e2["sgss_flag"].sum()
+    ecds_patient_count = e2["ecds_flag"].sum()
     icu_patient_count = e2["icu_flag"].sum()
     
     return e2, ecds_patient_count, sgss_patient_count, icu_patient_count
 
-e2, ecds_patient_count, sgss_patient_count, icu_patient_count = merge_data(ecds, p, n, d, icu)
+e2, sgss_patient_count, ecds_patient_count, icu_patient_count = merge_data(sgss, ecds, icu, cpns, ons)
 
 def ecds_percentage(x=1):
     return round(100*x/ecds_patient_count,0)
@@ -197,27 +244,13 @@ def sgss_percentage(x=1):
 def icu_percentage(x=1):
     return round(100*x/icu_patient_count,0)
 
-results, ecds_patient_count, sgss_patient_count, icu_patient_count = merge_data(ecds, p, n, d, icu)
-results.drop("Patient_ID", axis=1).sum()
-# -
-
-# # Weekly date cut-offs
-
-# +
-# create list of weekly dates to assess
-
-latest_date = max(ecds["EC_Conclusion_Date_latest"].max(), p["pos_Lab_Report_Date_latest"].max(), icu["IcuAdmissionDateTime_latest"].max())
-
-l = []
-for i in range(4):
-     l.append( [-i-1, latest_date + pd.Timedelta(days=-7*i-7)] )
-
-l = pd.DataFrame(l, columns=["week_no", "date"])
-l
+e2.drop("Patient_ID", axis=1).sum()
 
 
 # +
-   
+def days_ago(DT=datetime(2020,3,3)):
+    return (date.today()-datetime.date(DT)).days
+
 def ecds_percentage2(X):
     return str(X)+" ("+str(ecds_percentage(X))+"%)"
 def sgss_percentage2(X):
@@ -265,11 +298,12 @@ r = results_table(e2, ecds_patient_count, sgss_patient_count, icu_patient_count,
 # add previous weeks' summaries
 for i,w in enumerate(l["date"]): 
     ecds1 = ecds.loc[ecds["EC_Conclusion_Date_latest"] <= w]
-    p1 = p.loc[p["pos_Lab_Report_Date_latest"] <= w]
-    n1 = n.loc[n["neg_Lab_Report_Date_latest"] <= w]
+    sgss1 = sgss.loc[sgss["pos_Lab_Report_Date_latest"] <= w]
     icu1 = icu.loc[icu["IcuAdmissionDateTime_latest"] <= w]
+    cpns1 = cpns.loc[cpns["DateOfDeath"] <= w]
+    ons1 = ons.loc[ons["dod"]<= w]
     
-    e2, ecds_patient_count, sgss_patient_count, icu_patient_count = merge_data(ecds1, p1, n1, d, icu1)
+    e2, ecds_patient_count, sgss_patient_count, icu_patient_count = merge_data(sgss1, ecds1, icu1, cpns1, ons1)
     
     r0 = results_table(e2, ecds_patient_count, sgss_patient_count, icu_patient_count, w)
     r = r.merge(r0, suffixes=["","_"], left_index=True, right_index=True)
